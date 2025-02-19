3f21ddbf2d3430ecb296d52417894b248cc7554d
SoorajModi <soomodi@gmail.com>
2025-02-19 17:09:43 -0500

    Add autocomplete tree

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import express, { Request, Response , Application } from 'express';
+import express, { Express } from "express";
 import healthRoutes from './routes/health';
 import searchRoutes from './routes/search';
 
-const app: Application = express();
-const port = 3000;
+const app: Express = express();
+const port = 3001;
 
 app.use("/health", healthRoutes);
 app.use("/search", searchRoutes);
 
 app.listen(port, () => {
-  console.log(`[server]: Server has started at http://localhost:${port}`);
+    console.log(`[server]: Server is running at http://localhost:${port}`);
 });
